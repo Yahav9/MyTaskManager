1b9aeaffcd58796e1f86c35b4481ad28970645ca
Yahav <yahav9@cyber4s.dev>
2022-09-22 14:29:54 +0300

    start over

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,50 +1,21 @@
-import { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [placeholder, setPlaceholder] = useState('');
+  const [message, setMessage] = useState("");
 
-  const onInputChange = event => {
-    setUsername(event.target.value);
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('/data');
+      setMessage(result.data.message);
+    };
 
-  const tryLogIn = async event => {
-    event.preventDefault();
-    const data = await axios.get(`http://localhost:4000/users?name=${username}`)
-    if (data.data.length < 1) {
-      setUsername('')
-      setPlaceholder('Oops! Try Again')
-    } else {
-      window.location.href = '/' + data.data[0].name
-    }
-  }
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>Welcome to My Tasks Manager (or TODO)</h1>
-      <h4>Probably the Best Tasks Manager in the World</h4>
-      <h3>
-        Please enter your username to proceed or
-        <button onClick={() => {
-          window.open(
-            "/sign-up",
-            "_blank",
-            "height=500,width=500,top=250,left=500"
-          )
-        }}>sign up</button>
-        if you're not signed yet!
-      </h3>
-      <form>
-        <label>Enter Username: </label>
-        <input
-          value={username}
-          type="text"
-          onChange={onInputChange}
-          placeholder={placeholder} />
-        <button onClick={tryLogIn}>Log in</button>
-      </form>
-    </div>
+    <div>{message}</div>
   );
 }
 
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,19 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
 import App from './App';
-import SignUp from './routes/SignUp/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/*" element={<div>hello world</div>} />
-        </Routes>
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
