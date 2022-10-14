c4fb802dfe149099f9de833c50fd3380a82bc4d0
Yahav <yahav9@cyber4s.dev>
2022-10-14 13:40:31 +0300

    dealing with PUT requests to "/lists"

--- src/server/controllers/lists.ts
+++ src/server/controllers/lists.ts
@@ -44,6 +44,38 @@ export async function getLists(req: Request, res: Response) {
         const lists = await List.find({ user: new mongoose.Types.ObjectId(req.params.userId) });
         return res.json(lists);
     } catch (e) {
-        return res.json({ message: 'Wrong user-id' });
+        return res.json({ message: 'Wrong user id' });
+    }
+}
+
+export async function changeListName(req: Request, res: Response) {
+    const newName = req.body.name;
+
+    let existingListName;
+    try {
+        existingListName = await List.findOne({ name: newName });
+    } catch (e) {
+        return res.json({ error: e });
+    }
+    if (existingListName) {
+        return res.json({ message: 'List name already exist' });
+    }
+
+    let list;
+    try {
+        list = await List.findById(new mongoose.Types.ObjectId(req.params.listId));
+    } catch (e) {
+        return res.json({ error: e });
+    }
+    if (!list) {
+        return res.json({ message: 'wrong list id' });
+    }
+
+    list.name = newName;
+    try {
+        await list.save();
+        return res.json({ list });
+    } catch (e) {
+        return res.json({ error: e });
     }
 }
--- src/server/routes/lists.ts
+++ src/server/routes/lists.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { createList, getLists } from '../controllers/lists';
+import { changeListName, createList, getLists } from '../controllers/lists';
 
 const router = Router();
 
 router
     .get('/:userId', getLists)
     .post('/:userId', createList)
-// .put('/:userId/:listId', changeListName)
+    .put('/:listId', changeListName)
 // .delete('/:userId', deleteList);
 
 export default router;
