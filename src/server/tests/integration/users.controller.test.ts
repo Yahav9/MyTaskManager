a11a5226ae2ec137ad7310f54420c64eafa036f5
Yahav <yahav9@cyber4s.dev>
2023-04-30 13:10:15 +0300

    write integration tests for '/users/login' endpoint

--- src/server/tests/integration/users.controller.test.ts
+++ src/server/tests/integration/users.controller.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../../app';
 import newUser from '../mock-data/users/new-user.json';
 import existingUser from '../mock-data/users/existing-user.json';
+import wrongPasswordUser from '../mock-data/users/wrong-password-user.json';
 import User from '../../models/User';
 
 const URL = '/api/users';
@@ -25,5 +26,30 @@ describe(URL + '/signup', () => {
             .post(URL + '/signup')
             .send(existingUser);
         expect(response.body.message).toStrictEqual('Username already exist');
+    });
+});
+
+describe(URL + '/login', () => {
+    it('should return a json object that contains user id and token', async () => {
+        const response = await request(app)
+            .post(URL + '/login')
+            .send(existingUser);
+        expect(response.body).toBeInstanceOf(Object);
+        expect(response.body).toHaveProperty('userId');
+        expect(response.body).toHaveProperty('token');
+    });
+
+    it('should return an error message if username is wrong', async () => {
+        const response = await request(app)
+            .post(URL + '/login')
+            .send(newUser);
+        expect(response.body.message).toStrictEqual('wrong username');
+    });
+
+    it('should return an error message if password is wrong', async () => {
+        const response = await request(app)
+            .post(URL + '/login')
+            .send(wrongPasswordUser);
+        expect(response.body.message).toStrictEqual('Incorrect password');
     });
 });
