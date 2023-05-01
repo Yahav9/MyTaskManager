688a96548090dae82644a51a3eb6128d4771b725
Yahav <yahav9@cyber4s.dev>
2023-05-01 16:19:22 +0300

    refactor app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -7,9 +7,10 @@ import users from './routes/users';
 import lists from './routes/lists';
 import tasks from './routes/tasks';
 import { checkAuth } from './middleware/checkAuth';
-
 dotenv.config();
 
+const port = process.env.PORT || '4000';
+const connectionString = process.env.URL || 'no connection string';
 const app = express();
 
 app.use(cors())
@@ -24,17 +25,30 @@ app.use('/api/users', users)
     .use('/api/lists', lists)
     .use('/api/tasks', tasks);
 
-const port = process.env.PORT || '4000';
-const connectionString = process.env.URL || 'no connection string';
-
-mongoose.connect(connectionString)
-    .then(() => {
-        app.listen(port, () => {
-            console.log('Hosted: http://localhost:' + port);
-            console.log('connected to db');
-        });
-    })
-    .catch((e: Error) => {
-        console.log('could not connect to db', e);
+export async function connectToDataBase() {
+    mongoose.set('strictQuery', false);
+    try {
+        await mongoose.connect(connectionString);
+    } catch (err) {
+        console.log('could not connect to db, error: ', err);
         console.log('connectionString: ', connectionString);
+    }
+}
+
+export async function listenToPort() {
+    app.listen(port, () => {
+        console.log('Hosted: http://localhost:' + port);
+        console.log('connected to db');
     });
+}
+
+function runServer() {
+    if (process.env.NODE_ENV !== 'test') {
+        connectToDataBase();
+        listenToPort();
+    }
+}
+
+export default app;
+
+runServer();
