7acbdbe5cc36665c4e30f585c3c1c1958e3846e6
Yahav <yahav9@cyber4s.dev>
2023-05-01 16:30:22 +0300

    write integration test for POST requests to "/lists" endpoint

---
+++ src/server/tests/integration/lists.controller.test.ts
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import app, { connectToDataBase } from '../../app';
+import List from '../../models/List';
+import newList from '../mock-data/lists/new-list.json';
+import existingList from '../mock-data/lists/existing-list.json';
+import existingUser from '../mock-data/users/existing-user.json';
+import { Server } from 'http';
+
+const URL = '/api/lists/';
+let token: string;
+let userId: string;
+let server: Server;
+
+beforeAll((done) => {
+    server = app.listen(4002, async () => {
+        await connectToDataBase();
+        console.log('Server started');
+        done();
+    });
+});
+
+afterAll((done) => {
+    server.close(done);
+});
+
+afterEach(async () => {
+    await List.deleteOne({ name: newList.name });
+});
+
+beforeAll(async () => {
+    const response = await request(app)
+        .post('/api/users/login')
+        .send(existingUser);
+    token = response.body.token;
+    userId = response.body.userId;
+});
+
+describe(URL, () => {
+    test('POST ' + URL, async () => {
+        const response = await request(app)
+            .post(URL + userId)
+            .set('Authorization', token)
+            .send(newList);
+        expect(response.body).toBeInstanceOf(Object);
+        expect(response.body).toHaveProperty('_id');
+        expect(response.body).toHaveProperty('name');
+    });
+
+    test('GET ' + URL, async () => {
+        const response = await request(app)
+            .get(URL + userId)
+            .set('Authorization', token);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body[0].name).toStrictEqual(existingList.name);
+    });
+
+    // test('PATCH ' + URL, async () => { });
+
+    // test('DELETE ' + URL, async () => { });
+});
