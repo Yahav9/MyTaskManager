3c4e809f5a08fc61f37015b58a3d1521635b6000
Yahav <yahav9@cyber4s.dev>
2023-05-04 11:26:04 +0300

    refactor checkAuth.ts

--- src/server/middleware/checkAuth.ts
+++ src/server/middleware/checkAuth.ts
@@ -2,15 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 
 export function checkAuth(req: Request, res: Response, next: NextFunction) {
-    let token;
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.json({ message: 'Authentication failed!!' });
+    }
+
     try {
-        token = req.headers.authorization;
-        if (!token) {
-            return res.json({ message: 'Authentication failed!!' });
-        }
-        let decodedToken;
-        process.env.KEY && (decodedToken = jwt.verify(token, process.env.KEY));
-        // @ts-ignore
+        const key = process.env.KEY as string;
+        const decodedToken = jwt.verify(token, key) as jwt.JwtPayload;
         req.userId = decodedToken.userId;
         next();
     } catch (e) {
