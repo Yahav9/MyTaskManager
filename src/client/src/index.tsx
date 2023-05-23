8768bc200f3f4e8de16098c21ba8eb8ae9f1a1d8
Yahav <yahav9@cyber4s.dev>
2023-05-23 17:29:04 +0300

    small type specification in index.tsx

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
 root.render(<App />);
