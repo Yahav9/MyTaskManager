4ab2fe5850cd982d26eaa8bedfa151b6d4541678
Yahav <yahav9@cyber4s.dev>
2023-05-25 13:47:35 +0300

    create IAuthContext and fix authContext problems

--- src/client/src/shared/context/AuthContext.ts
+++ src/client/src/shared/context/AuthContext.ts
@@ -1,8 +1,17 @@
 import { createContext } from 'react';
 
-export const AuthContext = createContext({
-    userId: null,
-    token: null,
-    login: () => { },
-    logout: () => { }
+export type TLogin = (userId: string, token: string, expirationDate?: Date) => void
+
+interface IAuthContext {
+    userId?: string;
+    token?: string;
+    login?: TLogin;
+    logout?: () => void
+}
+
+export const AuthContext = createContext<IAuthContext>({
+    userId: undefined,
+    token: undefined,
+    login: undefined,
+    logout: undefined
 });
